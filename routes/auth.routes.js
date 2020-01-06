3ae4f179c2d3c71558742a511bd61c1129da8a4b
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-07 00:48:30 +0300

    login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -52,6 +54,59 @@ router.post(
 );
 
 // /api/auth/login
-router.post("/login", async (req, res) => {});
+router.post(
+	"/login",
+	[
+		check("email", "Enter velid email")
+			.normalizeEmail()
+			.isEmail(),
+		check("password", "Enter password").exists(),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					message: "Incorrect user data",
+					errors: errors.array(),
+				});
+			}
+
+			const { email, password } = req.body;
+
+			const user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({
+					message: "User not found",
+				});
+			}
+
+			const isPasswordMath = await bcrypt.compare(password, user.password);
+			if (!isPasswordMath) {
+				return res.status(400).json({
+					message: "Incorrect password. TTry again...",
+				});
+			}
+
+			const token = jwt.sign(
+				{
+					useerId: user.id,
+				},
+				config.get("jwtSecret"),
+				{
+					expiresIn: "1h",
+				}
+			);
+
+			res.status(200).json({ token, userId: user.id });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: "Login user error",
+			});
+		}
+	}
+);
 
 module.exports = router;
