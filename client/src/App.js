d50edafc282fa5c5c3400c54d6ed1c51a00e834d
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-08 00:49:59 +0300

    base routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "materialize-css";
 
+import { useRoutes } from "./routes";
+
 function App() {
+	const routes = useRoutes(false);
+
 	return (
-		<div className="container">
-			<h1>Hello World!</h1>
-		</div>
+		<Router>
+			<div className="container">{routes}</div>
+		</Router>
 	);
 }
 
