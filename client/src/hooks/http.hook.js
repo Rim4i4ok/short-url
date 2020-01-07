84faf8bdcb300f74599035d16d62d598ba449fff
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-08 01:28:18 +0300

    useHttp custom hook

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,38 @@
+import { useState, useCallback } from "react";
+
+export const useHttp = () => {
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const request = useCallback(
+		async (url, method = "GET", body = null, headers = {}) => {
+			setLoading(true);
+			try {
+				const response = await fetch(url, {
+					method,
+					body,
+					headers,
+				});
+
+				const data = await response.json();
+				if (!response.ok) {
+					throw new Error(data.message || "Somthing was wrong...");
+				}
+
+				setLoading(false);
+
+				return data;
+			} catch (error) {
+				setLoading(false);
+				setError(error.message);
+
+				throw error;
+			}
+		},
+		[]
+	);
+
+	const clearError = () => setError(null);
+
+	return { loading, request, error, clearError };
+};
