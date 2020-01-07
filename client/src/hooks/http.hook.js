9bad21b3f56b216cc9ad36dcf3fdde43c079dafa
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-08 01:37:12 +0300

    useHttp hook body fix

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -8,6 +8,11 @@ export const useHttp = () => {
 		async (url, method = "GET", body = null, headers = {}) => {
 			setLoading(true);
 			try {
+				if (body) {
+					body = JSON.stringify(body);
+					headers["Content-Type"] = "application/json";
+				}
+
 				const response = await fetch(url, {
 					method,
 					body,
