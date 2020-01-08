521dfb0936bb66d2027a77687939acb133193fef
Rim4i4ok <rim4i4ok@gmail.com>
2020-01-08 15:47:11 +0300

    context for auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "materialize-css";
 
 import { useRoutes } from "./routes";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-	const routes = useRoutes(false);
+	const { token, login, logout, userId } = useAuth();
+	const isAuthenticated = !!token;
+
+	const routes = useRoutes(isAuthenticated);
 
 	return (
-		<Router>
-			<div className="container">{routes}</div>
-		</Router>
+		<AuthContext.Provider
+			value={{
+				token,
+				login,
+				logout,
+				userId,
+				isAuthenticated,
+			}}
+		>
+			<Router>
+				<div className="container">{routes}</div>
+			</Router>
+		</AuthContext.Provider>
 	);
 }
 
